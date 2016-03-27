cf95307e50fb134da96e926079e0c3e9950dcd9a
Hugo Kerstens <hugokk@hotmail.nl>
2016-03-27 22:58:39 +0200

    Add more protocols to the /viaversion list command

--- src/main/java/us/myles/ViaVersion/commands/ViaVersionCommand.java
+++ src/main/java/us/myles/ViaVersion/commands/ViaVersionCommand.java
@@ -1,6 +1,7 @@
 package us.myles.ViaVersion.commands;
 
 import io.netty.util.ResourceLeakDetector;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
@@ -9,8 +10,10 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import us.myles.ViaVersion.ViaVersionPlugin;
 import us.myles.ViaVersion.api.ViaVersion;
+import us.myles.ViaVersion.api.protocol.ProtocolRegistry;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -25,18 +28,20 @@ public class ViaVersionCommand implements CommandExecutor {
                 sendHelp(sender);
             } else if (args.length == 1) {
                 if (args[0].equalsIgnoreCase("list")) {
-                    List<String> portedPlayers = new ArrayList<>();
-                    List<String> normalPlayers = new ArrayList<>();
+                    HashMap<Integer, List<Player>> protocols = new HashMap<Integer, List<Player>>();
+                    for(ProtocolNames pn : ProtocolNames.values()) {
+                        protocols.put(pn.getId(), new ArrayList<Player>());
+                    }
                     for (Player p : Bukkit.getOnlinePlayers()) {
                         if (ViaVersion.getInstance().isPorted(p)) {
-                            portedPlayers.add(p.getName());
+                            protocols.get(ViaVersion.getInstance().getPlayerVersion(p)).add(p);
                         } else {
-                            normalPlayers.add(p.getName());
+                            protocols.get(ProtocolRegistry.SERVER_PROTOCOL).add(p);
                         }
                     }
-
-                    sender.sendMessage(color("&8[&61.9&8]: &b" + portedPlayers.toString()));
-                    sender.sendMessage(color("&8[&61.8&8]: &b" + normalPlayers.toString()));
+                    for(Integer i : protocols.keySet()) {
+                        sender.sendMessage(color("&8[&6" + ProtocolNames.getById(i).getName() + "&8]: &b" + protocols.get(i).toString()));
+                    }
                 } else if (args[0].equalsIgnoreCase("debug")) {
                     plugin.setDebug(!plugin.isDebug());
                     sender.sendMessage(color("&6Debug mode is now " + (plugin.isDebug() ? "&aenabled" : "&cdisabled")));
@@ -78,5 +83,28 @@ public class ViaVersionCommand implements CommandExecutor {
 
     public String color(String string) {
         return string.replace("&", "§");
+    }
+
+    @RequiredArgsConstructor
+    private enum ProtocolNames {
+
+        V1_7_1(4, "1.7.1"),
+        V1_7_6(5, "1.7.6"),
+        V1_8(47, "1.8"),
+        V1_9(107, "1.9"),
+        V1_9_1_PRE2(108, "1.9.1-pre2");
+
+        @Getter
+        private final int id;
+        @Getter
+        private final String name;
+
+        public static ProtocolNames getById(int id) {
+            for(ProtocolNames pn : ProtocolNames.values()) {
+                if(pn.getId() == id)
+                    return pn;
+            }
+            return null;
+        }
     }
 }
